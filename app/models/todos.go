f3215e3128367d717db3525f647d7db1bbd02aeb
tomotakashimizu <tomotaka.developer@gmail.com>
2022-07-21 18:36:14 +0900

    create todo

--- app/models/todos.go
+++ app/models/todos.go
@@ -1,10 +1,24 @@
 package models
 
-import "time"
+import (
+	"time"
+)
 
 type Todo struct {
 	ID        int
 	Content   string
 	UserID    int
 	CreatedAt time.Time
+}
+
+func (t *Todo) CreateTodo() error {
+	cmd := `INSERT INTO todos(
+		content,
+		user_id,
+		created_at)
+		values (?, ?, ?)
+		`
+
+	_, err := Db.Exec(cmd, t.Content, t.UserID, time.Now())
+	return err
 }
--- main.go
+++ main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/tomotakashimizu/todoapp-go/app/models"
 )
 
 func main() {
 	fmt.Println(models.Db)
+
+	t := models.Todo{Content: "Todofor2_2", UserID: 2}
+	fmt.Println(t)
+
+	err := t.CreateTodo()
+	if err != nil {
+		log.Fatalf("failed to Create Todo: %s", err.Error())
+	}
 }
