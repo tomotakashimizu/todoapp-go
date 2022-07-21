3cf2982cd4a9c27af1e4e83318a09c8d5935f4fe
tomotakashimizu <tomotaka.developer@gmail.com>
2022-07-21 19:06:35 +0900

    update todo, delete todo, GetTodosByUser

--- app/models/todos.go
+++ app/models/todos.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"fmt"
+	"log"
 	"time"
 )
 
@@ -53,4 +54,49 @@ func GetAllTodos() ([]Todo, error) {
 	}
 
 	return todos, nil
+}
+
+func (u *User) GetTodosByUser() (todos []Todo, err error) {
+	cmd := `select id, content, user_id, created_at from todos
+	where user_id = ?`
+
+	rows, err := Db.Query(cmd, u.ID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for rows.Next() {
+		var todo Todo
+		err = rows.Scan(
+			&todo.ID,
+			&todo.Content,
+			&todo.UserID,
+			&todo.CreatedAt)
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+		todos = append(todos, todo)
+	}
+	rows.Close()
+
+	return todos, err
+}
+
+func (t *Todo) UpdateTodo() error {
+	cmd := `update todos set content = ?, user_id = ? 
+	where id = ?`
+	_, err := Db.Exec(cmd, t.Content, t.UserID, t.ID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
+
+func (t *Todo) DeleteTodo() error {
+	cmd := `delete from todos where id = ?`
+	_, err := Db.Exec(cmd, t.ID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
 }
