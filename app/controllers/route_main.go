e892a136bc892c19a63f95c1e1c22511410f4eeb
tomotakashimizu <tomotaka.developer@gmail.com>
2022-07-21 22:26:02 +0900

    view page using renderTemplate

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -1,7 +1,7 @@
 package controllers
 
 import (
-	"fmt"
+	"html/template"
 	"io/ioutil"
 	"net/http"
 )
@@ -25,11 +25,24 @@ func LoadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, err := template.ParseFiles("app/views/" + tmpl + ".html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	// w に p の値を使って html を表示
+	t.Execute(w, p)
+}
+
 func viewHandler(w http.ResponseWriter, r *http.Request) {
+	// URL の /view/ 以降の文字列を title に代入
 	title := r.URL.Path[len("/view/"):]
+	// title.txt の file があれば p に情報を代入
 	p, err := LoadPage(title)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+	// w に 値を書き込んで表示
+	// fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+	renderTemplate(w, "view", p)
 }
--- app/controllers/server.go
+++ app/controllers/server.go
@@ -10,7 +10,9 @@ func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
 	http.Handle("/static/", http.StripPrefix("/static/", files))
 
+	// URL /view/ の処理を関数で定義
 	http.HandleFunc("/view/", viewHandler)
 
+	// サーバを起動
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
